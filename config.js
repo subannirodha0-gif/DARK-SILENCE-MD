6693d73ac42ce6cd6ccf5ead4407243630722283
subannirodha0-gif <subannirodha0@gmail.com>
2025-11-19 12:31:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~eF9xAbjS#osChzEV8WFJoz-dMSjeapUAgz7hX1L6ynqSEs9vYfoU",
     CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
